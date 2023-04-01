711f5661427958c922e7294d80f35fa721f4bf19
https://github.com/marija-rajak <mrajchica@gmail.com>
2023-04-01 14:43:39 +0200

    Calcualtion funcionality added

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 const digitKeys = document.getElementsByClassName('digit');
-
 const operatorKeys = document.getElementsByClassName('operator');
-
 const calculationBtn = document.getElementById('calculateBtn');
+const resetBtn = document.getElementById('resetBtn');
+const deleteBtn = document.getElementById('deleteBtn');
 
 let display = document.getElementById('display');
 
 let tempNumber;
-let startNumber =0;
+let startNumber;
 let nextNumber;
 let result;
 
@@ -21,6 +21,7 @@ for (let i = 0; i < operatorKeys.length; i++) {
 	operatorKeys[i].addEventListener('click', function () {
 		calculate();
 		getOperator(this.value);
+		tempNumber = undefined;
 
 	});
 }
@@ -30,32 +31,50 @@ calculationBtn.addEventListener('click', function () {
 	operator = undefined;
 });
 
+//Reset button resets expression, display and start` number
+resetBtn.addEventListener('click', function () {
+	resetExpression();
+	startNumber = undefined;
+	display.innerText = "";
+});
+
+//
+deleteBtn.addEventListener('click', resetNumber);
+
+//Collects digits user inputs and converts it into number, assign value to a proper number
 function typeNumber() {
-	if (!tempNumber) {
+	if (!tempNumber && tempNumber !== 0) {
 		display.innerText = this.value;
 	} else {
 		display.innerText += this.value;
 	}
 
 	tempNumber = parseFloat(display.innerText);
+	console.log(tempNumber);
 	getNumber();
 }
 
 function getOperator(value) {
-	operator = value;
+	if (startNumber || startNumber === 0) {
+		operator = value;
+	}
 }
 
+//Stores temp as proper number, depending on calculation progress
 function getNumber() {
 	if (!operator) {
 		startNumber = tempNumber;
+		console.log('startNumber=' + startNumber);
 	} else {
 		nextNumber = tempNumber;
+		console.log('nextNumber=' + nextNumber);
 	}
-	tempNumber = undefined;
 }
 
+//Performs calculation and assign value to result, displays result
 function calculate() {
 	if ((startNumber || startNumber === 0) && operator && (nextNumber || nextNumber === 0)) {
+		console.log(startNumber + operator + nextNumber + "=")
 		switch (operator) {
 			case '+':
 				result = startNumber + nextNumber;
@@ -69,15 +88,39 @@ function calculate() {
 			case '/':
 				result = startNumber / nextNumber;
 		}
-		if (Number.isFinite(result)) {
-			display.innerText = result;
-			startNumber = result;
-
-		} else {
-			display.innerText = "error!";
-			startNumber = undefined;
-			//reset();
-		}
-		console.log(result);
+		setNextCalculation();
 	}
+}
+
+//Displays result and resets expression elements
+function setNextCalculation() {
+	if (Number.isFinite(result)) {
+		display.innerText = result;
+		startNumber = result;
+
+	} else {
+		display.innerText = "error!";
+		startNumber = undefined;
+	}
+
+	resetExpression();
+}
+
+//Resets numbers (temp and next), operator and result to initial value
+function resetExpression() {
+	nextNumber = undefined;
+	result = undefined;
+	tempNumber = undefined;
+	operator = undefined;
+}
+
+//Resets last set number
+function resetNumber() {
+	if (nextNumber || nextNumber === 0) {
+		nextNumber = undefined;
+	} else if (startNumber || startNumber === 0){
+		startNumber = undefined;
+	}
+	display.innerText = '';
+	console.log(123);
 }
